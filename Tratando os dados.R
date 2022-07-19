420e1a4d9a87753153220bb1e2e043227a220b1e
João Lucas <joaolucas.moreira.moura@gmail.com>
2022-07-18 22:47:58 -0300

    Pequenas mudanças no encoding, entre outras.

--- Tratando os dados.R
+++ Tratando os dados.R
@@ -42,13 +42,15 @@ for (i in arquivos_zip) {
 
 # Se for para concatenar todos os tipos de dre disponibilizados pela CVM usar codigo abaixo
 #nomes <- c("DVA_ind", "DVA_con", "DRE_ind", "DRE_con", "DRA_ind", "DMPL_ind", "DMPL_con", "DFC_MI_ind", "DFC_MI_con", "DFC_MD_ind", "DFC_MD_con", "BPP_ind", "BPP_con", "BPA_ind", "BPA_con")
-nome <- "DRE_con"
+nome <- "DRE_ind"
 hist_dre <- data.frame()
 for (ano in 2011:2021) {
   #arquivo <- data.frame()
   #  for (nome in nomes) { 
-  hist_dre <- rbind(hist_dre, read.csv2(paste("dados_CVM_csv/itr_cia_aberta_", nome,"_", ano, ".csv", sep = "")))
+  hist_dre <- rbind(hist_dre, read_csv2(paste("dados_CVM_csv/itr_cia_aberta_", nome,"_", ano, ".csv", sep = ""),
+                                       locale = locale(encoding = "ISO-8859-1")))
 }
+
 
 # Criando histórico de reclamações do BACEN 
 hist_reclam <- data.frame()
@@ -59,6 +61,8 @@ for (n in 1:20) {
                                   trim_ws = TRUE))
 }
 
+#summary(hist_reclam)
+
 
 hist_reclam_2 <- data.frame()
 for (n in 6:25) {
@@ -68,6 +72,8 @@ for (n in 6:25) {
                                          trim_ws = TRUE))
 }
 
+#summary(hist_reclam_2)
+
 # Renomeando o nome das colunas 
 colnames(hist_reclam_2) <- c("Ano", "Trimestre", "Categoria", "Tipo", "CNPJ.IF", "Instituição.financeira", "Índice", "Quantidade.de.reclamações.reguladas.procedentes", "Quantidade.de.reclamações.reguladas...outras", "Quantidade.de.reclamações.não.reguladas", "Quantidade.total.de.reclamações", "Quantidade.total.de.clientes...CCS.e.SCR", "Quantidade.de.clientes...CCS", "Quantidade.de.clientes...SCR", "Quantidade.de.clientes...FGC", "X")
 
@@ -76,8 +82,8 @@ colnames(hist_reclam) <- c("Ano","Trimestre","Categoria","Tipo","CNPJ.IF","Insti
 # Adicionando a variavel categoria_2, se empresa maior que 4 milhões
 hist_reclam_2$Categoria_2 <- hist_reclam_2$Quantidade.total.de.clientes...CCS.e.SCR>4000000
 hist_reclam_2$Categoria_2[is.na(hist_reclam_2$Categoria_2)] <- FALSE
-hist_reclam_2$Categoria_2 <- replace(hist_reclam_2$Categoria_2, hist_reclam_2$Categoria_2 == "FALSE", "Menos de 4 milhões de clientes")
-hist_reclam_2$Categoria_2 <- replace(hist_reclam_2$Categoria_2, hist_reclam_2$Categoria_2 == "TRUE", "Mais de 4 milhões de clientes")
+hist_reclam_2$Categoria_2 <- replace(hist_reclam_2$Categoria_2, hist_reclam_2$Categoria_2 == "FALSE", "Menos de 4 milhoes de clientes")
+hist_reclam_2$Categoria_2 <- replace(hist_reclam_2$Categoria_2, hist_reclam_2$Categoria_2 == "TRUE", "Mais de 4 milhoes de clientes")
 
 
 # Tirando o simbolo de numero do 1 e 2  data frame
@@ -86,23 +92,31 @@ hist_reclam_2$Trimestre = gsub("\\º","",hist_reclam_2$Trimestre)
 hist_reclam$Trimestre = gsub("\\º","",hist_reclam$Trimestre)
 
 #Tentando inserir uma coluna de data no dataframe 1 e 2
-hist_reclam_2 <- hist_reclam_2 %>% 
-  mutate(tri = ifelse(Trimestre == 2, 4, 
-                      ifelse(Trimestre == 3, 7,
-                             ifelse(Trimestre == 4, 10, 1)))) %>% 
-  mutate(date = as.Date(paste0(tri, '-', Trimestre, '-', Ano), format = "%m-%d-%Y" ))
+
+
+hist_reclam_2$tri <- ifelse(hist_reclam_2$Trimestre == 2, 4, 
+                            ifelse(hist_reclam_2$Trimestre == 3, 7,
+                                   ifelse(hist_reclam_2$Trimestre == 4, 10, 1)))
+hist_reclam_2$date <- as.Date(paste0(hist_reclam_2$tri, '-', 1, '-', hist_reclam_2$Ano), format = "%m-%d-%Y")
+
 
 hist_reclam <- hist_reclam %>% 
   mutate(tri = ifelse(Trimestre == 2, 4, 
                       ifelse(Trimestre == 3, 7,
                              ifelse(Trimestre == 4, 10, 1)))) %>% 
-  mutate(date = as.Date(paste0(tri, '-', Trimestre, '-', Ano), format = "%m-%d-%Y" ))
+  mutate(date = as.Date(paste0(tri, '-', 1, '-', Ano), format = "%m-%d-%Y" ))
 
 # Inserindo formato de data com Trimestre Quarter/year
 
 hist_reclam_2$date_2 <- as.yearqtr(hist_reclam_2$date, format = "%m-%d-%Y")
 
 hist_reclam$date_2 <- as.yearqtr(hist_reclam$date, format = "%m-%d-%Y")
+
+#Retirando colunas vazias
+hist_reclam <- hist_reclam[,-c(12)]
+
+hist_reclam_2 <- hist_reclam_2[,-c(16,17)]
+
 
 write.csv2(hist_reclam, "hist_reclam", row.names = FALSE)
 write.csv2(hist_reclam_2, "hist_reclam_2", row.names = FALSE)
